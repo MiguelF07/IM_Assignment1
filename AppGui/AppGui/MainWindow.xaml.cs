95c713b31e160ab567a9b3594ec617ba5705a2be
Miguel Ferreira <miguel.r.ferreira@ua.pt>
2022-11-14 16:26:44 +0000

    added cards and reading of hand

--- AppGui/AppGui/MainWindow.xaml.cs
+++ AppGui/AppGui/MainWindow.xaml.cs
@@ -11,6 +11,9 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using System.Windows.Controls;
 using OpenQA.Selenium.Interactions;
+using System.Xml;
+using System.Collections.Generic;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace AppGui
 {
@@ -26,12 +29,14 @@ namespace AppGui
         private LifeCycleEvents lce;
         private MmiCommunication mmic;
         private IWebDriver driver;
+        private Dictionary<String, String> cards;
 
         public MainWindow()
         {
             InitializeComponent();
             driver = new ChromeDriver("../."); //Uses a specific driver for chrome version 107
             driver.Navigate().GoToUrl("https://www.playgreatpoker.com/FreePokerGameStart.html");
+            cards = addCards();
 
 
             mmiC = new MmiCommunication("localhost",8000, "User1", "GUI");
@@ -54,6 +59,18 @@ namespace AppGui
             var com = doc.Descendants("command").FirstOrDefault().Value;
             dynamic json = JsonConvert.DeserializeObject(com);
             Console.WriteLine(((string)json.recognized[0].ToString()));
+
+
+            //Console.WriteLine("jjjjjjjjj");
+            //string value = doc.Root.Element("emma:interpretation").Attribute("emma:confidence").Value;
+            //Console.WriteLine("-----------");
+            //Console.WriteLine(value);
+            //if (float.Parse(value) < 0.5)
+            //{
+            //    call_tts("Desculpe pode repetir?");
+            //}
+
+
 
             switch ((string)json.recognized[0].ToString())
             {
@@ -137,9 +154,14 @@ namespace AppGui
                     //informação das cartas na mesa
                     break;
                 case "HAND":
-                    //informação das cartas na mão
+                    cardsInTable();
                     break;
                 case "POT":
+                    if (driver.FindElements(By.Id("total-pot")).Count() > 0)
+                    {
+                        String pot_total = driver.FindElement(By.Id("total-pot")).Text;
+                        call_tts("O valor total apostado atualmente é " + pot_total.Split('$')[1]+"dólares");
+                    }
                     //procurar valor no id="total-pot"
                     break;
 
@@ -169,6 +191,81 @@ namespace AppGui
             */
             var exNot = lce.ExtensionNotification(0 + "", 0 + "", 1, json2);
             mmic.Send(exNot);
+        }
+
+        private void cardsInTable()
+        {
+            var textC1 = driver.FindElement(By.XPath("//*[@id=\"seat0\"]/div[1]/div[1]")).GetAttribute("style");
+            var card1 = textC1.Split('/')[2].Split('.')[0].ToString();
+
+            var textC2 = driver.FindElement(By.XPath("//*[@id=\"seat0\"]/div[1]/div[2]")).GetAttribute("style");
+            var card2 = textC2.Split('/')[2].Split('.')[0].ToString();
+
+            var textCardC1 = cards[card1];
+            var textCardC2 = cards[card2];
+
+            call_tts("A sua mão contem " + textCardC1 + " e " + textCardC2);
+
+            
+        }
+
+        private Dictionary<String,String> addCards()
+        {
+            var cards = new Dictionary<String, String>();
+            cards.Add("king_of_hearts", "rei de copas");
+            cards.Add("queen_of_hearts", "rainha de copas");
+            cards.Add("jack_of_hearts", "valete de copas");
+            cards.Add("10_of_hearts", "dez de copas");
+            cards.Add("9_of_hearts", "nove de copas");
+            cards.Add("8_of_hearts", "oito de copas");
+            cards.Add("7_of_hearts", "sete de copas");
+            cards.Add("6_of_hearts", "seis de copas");
+            cards.Add("5_of_hearts", "cinco de copas");
+            cards.Add("4_of_hearts", "quatro de copas");
+            cards.Add("3_of_hearts", "três de copas");
+            cards.Add("2_of_hearts", "dois de copas");
+            cards.Add("ace_of_hearts", "ás de copas");
+            cards.Add("king_of_diamonds", "rei de ouros");
+            cards.Add("queen_of_diamonds", "rainha de ouros");
+            cards.Add("jack_of_diamonds", "valete de ouros");
+            cards.Add("10_of_diamonds", "dez de ouros");
+            cards.Add("9_of_diamonds", "nove de ouros");
+            cards.Add("8_of_diamonds", "oito de ouros");
+            cards.Add("7_of_diamonds", "sete de ouros");
+            cards.Add("6_of_diamonds", "seis de ouros");
+            cards.Add("5_of_diamonds", "cinco de ouros");
+            cards.Add("4_of_diamonds", "quatro de ouros");
+            cards.Add("3_of_diamonds", "três de ouros");
+            cards.Add("2_of_diamonds", "dois de ouros");
+            cards.Add("ace_of_diamonds", "ás de ouros");
+            cards.Add("king_of_spades", "rei de espadas");
+            cards.Add("queen_of_spades", "rainha de espadas");
+            cards.Add("jack_of_spades", "valete de espadas");
+            cards.Add("10_of_spades", "dez de espadas");
+            cards.Add("9_of_spades", "nove de espadas");
+            cards.Add("8_of_spades", "oito de espadas");
+            cards.Add("7_of_spades", "sete de espadas");
+            cards.Add("6_of_spades", "seis de espadas");
+            cards.Add("5_of_spades", "cinco de espadas");
+            cards.Add("4_of_spades", "quatro de espadas");
+            cards.Add("3_of_spades", "três de espadas");
+            cards.Add("2_of_spades", "dois de espadas");
+            cards.Add("ace_of_spades", "ás de espadas");
+            cards.Add("king_of_clubs", "rei de paus");
+            cards.Add("queen_of_clubs", "rainha de paus");
+            cards.Add("jack_of_clubs", "valete de paus");
+            cards.Add("10_of_clubs", "dez de paus");
+            cards.Add("9_of_clubs", "nove de paus");
+            cards.Add("8_of_clubs", "oito de paus");
+            cards.Add("7_of_clubs", "sete de paus");
+            cards.Add("6_of_clubs", "seis de paus");
+            cards.Add("5_of_clubs", "cinco de paus");
+            cards.Add("4_of_clubs", "quatro de paus");
+            cards.Add("3_of_clubs", "três de paus");
+            cards.Add("2_of_clubs", "dois de paus");
+            cards.Add("ace_of_clubs", "ás de paus");
+
+            return cards;
         }
     }
 }
